90be81ea12dc45c9476ace135f528b72b712766d
davidyoon <qqpo12@naver.com>
2022-01-20 19:57:43 +0900

    make printBanner, printDetails functions

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 function printOwing(invoice) {
     let outstranding = 0;
 
-    console.log("****************")
-    console.log("**** 고객 채무 ****")
-    console.log("****************")
+    printBanner()
 
     for(const o of invoice.orders) {
         outstranding += o.amount;
@@ -13,8 +11,19 @@ function printOwing(invoice) {
     const today = Clock.today;
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-    console.log('고객명 : ${invoice.customer}')
-    console.log('채무액: ${outstanding}');
-    console.log('마감일: ${invoice.dueDate.toLocaleDateString()}');
+    printDetails();
+    
+
+    function printBanner() {
+        console.log("****************");
+        console.log("**** 고객 채무 ****");
+        console.log("****************");
+    }
+
+    function printDetails() {
+        console.log('고객명 : ${invoice.customer}')
+        console.log('채무액: ${outstanding}');
+        console.log('마감일: ${invoice.dueDate.toLocaleDateString()}');
+    }
     
 }
