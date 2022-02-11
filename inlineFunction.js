94a8edd1b8acae32f6e3b2c209f8591df1753574
davidyoon <qqpo12@naver.com>
2022-02-11 19:31:48 +0900

    add refactoring sample

--- inlineFunction.js
+++ inlineFunction.js
@@ -1,3 +1,14 @@
+// before
+function getRating(driver) {
+    return moreThanFiveLateDeliviries(driver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliviries(driver) {
+    return driver.numberOfLateDeliveries > 5;
+}
+
+
+// after
 function rating(aDriver) {
     return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
--- inlineFunction2.js
+++ inlineFunction2.js
@@ -1,6 +1,21 @@
+// ASIS
+function reportLines(aCustomer) {
+    const lines = [];
+    gatherCustomerData(lines, aCustomer);
+    return lines
+}
+
+function gatherCustomerData(out, aCustomer) {
+    out.push(["name", aCustomer.name]);
+    out.push(["location", aCustomer.location]);
+}
+
+
+// TOBE
 function reportLines(aCustomer) {
     const lines = [];
     lines.push(["name", aCustomer.name]);
     lines.push(["location",aCustomer.location]);
     return lines;
 }
+
