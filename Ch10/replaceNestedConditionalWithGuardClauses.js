7146ee5e1993c9ee346a73ff68a0cdf944ce126e
davidyoon <qqpo12@naver.com>
2022-03-31 19:45:12 +0900

    add example

--- Ch10/replaceNestedConditionalWithGuardClauses.js
+++ Ch10/replaceNestedConditionalWithGuardClauses.js
@@ -65,3 +65,82 @@ function payAmount(employee) {
     }
     return result;
 } 
+
+// Step 2  다음 조건으로 넘어간다
+function payAmount(employee) {
+    let result;
+    if (employee.isSeparated) return  {amount: 0, reasonCode: "SEP" };
+    if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
+    else {
+         //급여 계산 로직
+         lorem.ipsum(dolor.sitAmet);
+         consectetur(adipiscing).elit();
+         sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+         ut.enim.ad(minim.veniam);
+         result = someFinalComputation()
+    }
+    return result;
+} 
+// Step 3 아무일도 하지 않는 result는 제거한다
+function payAmount(employee) {
+    if (employee.isSeparated) return  {amount: 0, reasonCode: "SEP" };
+    if (employee.isRetired) return  {amount: 0, reasonCode: "RET"};
+    //급여 계산 로직
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    ut.enim.ad(minim.veniam);
+    return someFinalComputation()
+    
+} 
+
+// 예시 2 조건 반대로 만들기
+function adjustedCapital(anInstrument) {
+    let result = 0;
+    if (anInstrument.capital > 0) {
+        if ( anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+            result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+        }
+    }
+    return result;
+}
+
+// Step 1 첫 조건 보호구문으로 만든다.
+function adjustedCapital(anInstrument) {
+    let result = 0;
+    if (anInstrument.capital <= 0) return result;
+    if ( anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+        result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+    }
+    return result;
+}
+// Step 2 not 연산자(!) 추가
+function adjustedCapital(anInstrument) {
+    let result = 0;
+    if (anInstrument.capital <= 0) return result;
+    if (!(anInstrument.interestRate > 0 && anInstrument.duration > 0)) return result;
+    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+    return result;
+}
+
+// Step 3 간소화 not 제거
+function adjustedCapital(anInstrument) {
+    let result = 0;
+    if (anInstrument.capital <= 0) return result;
+    if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
+    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+    return result;
+}
+
+// Step 4 같은 결과를 내는 조건식 통합
+function adjustedCapital(anInstrument) {
+    let result = 0;
+    if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
+    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+    return result;
+}
+// Step 5 result 한 가지 일만 하도록 
+function adjustedCapital(anInstrument) {
+    if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return 0;
+    return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+}
