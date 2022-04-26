9aa69927df540a6a7c4ce7bf33a6ad6c4e2c3211
davidyoon <qqpo12@naver.com>
2022-04-26 19:03:49 +0900

    add example

---
+++ Ch12/pullUpField.java
@@ -0,0 +1,23 @@
+// ASIS
+class Employee {
+
+}
+
+class Salesperson extends Employee {
+    private String name;
+}
+
+class Engineer extends Employee {
+    private String name;
+}
+// TOBE
+class Employee {
+    protected String name;
+}
+
+class Salesperson extends Employee {
+}
+
+class Engineer extends Employee {
+    
+}
