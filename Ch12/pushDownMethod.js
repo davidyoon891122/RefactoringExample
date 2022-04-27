fd851fa8fb061d4c97805067d98fcec3ed3878ba
davidyoon <qqpo12@naver.com>
2022-04-27 19:53:27 +0900

    add example

---
+++ Ch12/pushDownMethod.js
@@ -0,0 +1,27 @@
+// ASIS
+class Employee {
+    get quota {
+    }
+}
+
+class Engineer extends Employee {
+
+}
+
+class Salesperson extends Employee {
+
+}
+
+// TOBE
+class Employee {
+
+}
+
+class Engineer extends Employee {
+
+}
+
+class Salesperson extends Employee {
+    get quota {
+    }
+}
